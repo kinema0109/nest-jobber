af5e29372646308ccc0ed848e949de7892d76b2a
thoqb123456@gmail.com <thoqb123456@gmail.com>
2025-03-22 19:01:49 +0700

    setup gprc server

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -8,10 +8,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth'; // Ensure this path is correct and the file exists
+import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-
   // Enable CORS
   app.enableCors({
     origin: true, // Allow all origins in development
@@ -22,6 +25,14 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.use(cookieParser());
   const port = app.get(ConfigService).getOrThrow('PORT');
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(process.cwd(), 'dist/apps/jobber-auth/proto/auth.proto'),
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
